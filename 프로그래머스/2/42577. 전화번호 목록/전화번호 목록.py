d40bbe2b4d7ebd03063be76ae7a0b7fd2a275f78
이명진 <50224952+septeratz@users.noreply.github.com>
2026-04-21 11:53:00 +0900

    [level 2] Title: 전화번호 목록, Time: 99.72 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,3 @@
 def solution(phone_book):
-    # 정렬하면 어차피 오름차순으로, 사전순으로 정렬되므로
-    # 바로 뒷쪽이랑만 검사하면 됨 
-    k = phone_book
-    k.sort()
-    for i in range(len(k) - 1):
-        if k[i+1].startswith(k[i]): return False
-    return True
+    phone_book.sort()
+    return not any(phone_book[i+1].startswith(phone_book[i]) for i in range(len(phone_book)-1))
